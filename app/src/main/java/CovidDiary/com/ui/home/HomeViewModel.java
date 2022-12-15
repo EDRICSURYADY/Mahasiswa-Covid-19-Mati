25782dc491942a990fe62b59a8170d15eb0b1da3
edricsuryady <edricsuryady@gmail.com>
2022-12-15 09:51:53 +0700

    Progres

---
+++ app/src/main/java/CovidDiary/com/ui/home/HomeViewModel.java
@@ -0,0 +1,18 @@
+package CovidDiary.com.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Data Covid19");
+    }
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
